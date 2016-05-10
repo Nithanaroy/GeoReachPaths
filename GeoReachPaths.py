f4924f4864759c35c036055cb46df8dd3757f02f
Nitin Pasumarthy <nithanaroy@gmail.com>
2016-05-09 21:23:20 -0700

    Completed range reach paths implementation

--- GeoReachPaths.py
+++ GeoReachPaths.py
@@ -1,4 +1,4 @@
-import math
+import math, heapq
 import networkx as nx
 from Common import extend_dictionary
 
@@ -13,6 +13,8 @@ class GeoReachPaths:
     def __init__(self, G, RF, M):
         """
         creates a new instance of GeoReachPaths
+        Every edge should have a weight attribute signifying the distance between the connecting vertices --> Used in (1)
+        Names of the nodes should be strings --> Used in (2)
         :param G: instance of NetworkX directed graph
         :param RF: reduction factor
         :param M: maximum number of entries per row in index table. Governs the size of index
@@ -39,6 +41,7 @@ class GeoReachPaths:
         # Add multi hop reachability
         self._do_dfs(G, index)
 
+        # Update reachability information to each node in the component
         for v in G.nodes():  # for each component
             for w in G.node[v]['members']:  # for each node in the component
                 try:
@@ -77,11 +80,110 @@ class GeoReachPaths:
             return set()
 
     def region(self, v):
-        r = GeoReachPaths._DEFAULT_RES * 1.0  # to enable floating division later
-        x_blocks = (self._MAX_LNG - self._MIN_LNG) / r
-        y_blocks = (self._MAX_LAT - self._MIN_LAT) / r
         lat = self.G.node[v]['spatial']['lat'] + 90
         lng = self.G.node[v]['spatial']['lng'] + 180
-        x_block_id = int(math.ceil(lng / x_blocks))
-        y_block_id = int(math.ceil(lat / y_blocks))
-        return x_block_id * r + y_block_id
+        return self._region_for(lat, lng)
+
+    def _region_for(self, lat, lng):
+        """
+        Finds the region ID for a given latitude and longitude
+        :return: region ID as an integer
+        """
+        r = GeoReachPaths._DEFAULT_RES * 1.0  # to enable floating division later
+        x_block_width = (self._MAX_LNG - self._MIN_LNG) / r
+        y_block_width = (self._MAX_LAT - self._MIN_LAT) / r
+        x_block_id = min(math.floor(lng / x_block_width), r - 1)
+        y_block_id = min(math.floor(lat / y_block_width), r - 1)
+        return int(x_block_id * r + y_block_id)
+
+    def range_reach_paths(self, s, R, K):
+        """
+        Finds the K shortest paths from s in region R
+        Index should be created using create_index() before calling this function
+        :param s: name of the source vertex
+        :param R: region of interest as a list of co-ordinates [nelat, nelong, swlat, swlong]
+        :param K: number of shortest paths required as a positive integer
+        :return: a list of K paths sorted by distance from s. Eg: [[1,2,3], [1,5,6]]
+        """
+        self._init(s)
+        Q = heapq.heapify([(self.G.node[v]['d'], v) for v in self.G.nodes()])  # Priority Q keyed by distance from s
+        R_in_blockids = self._region_for_latlng(R)
+        nearest_vertices = []
+        while len(Q) > 0:  # while Q is not empty
+            u = heapq.heappop(Q)  # u <- EXTRACT_MIN(Q)
+            if self._vertex_reaches(u, R_in_blockids):  # if u reaches the given region R
+                for v in self.G[u].keys():  # for each v in G.Adj(u)
+                    if self._vertex_lies_in(v, R):  # if v lies in the given region R
+                        nearest_vertices.append(v)  # collect the vertex
+                        if len(nearest_vertices) is K:  # if K vertices are collected
+                            return self._path_for(nearest_vertices)  # return the paths for each
+
+    def _path_for(self, vertices):
+        """
+        Finds the paths for each vertex in the list of vertices
+        :param vertices: list of vertices
+        :return: a list of paths. Eg: [[1,2,3], [4,5,6]]
+        """
+        paths = []
+        for v in vertices:
+            path = []
+            is_source = False
+            while not is_source:
+                path.insert(0, v)
+                v = self.G.node[v]['p']
+                is_source = v is None
+            paths.append(path)
+        return paths
+
+    def _init(self, s):
+        for v in self.G.nodes():  # for each vertex in G
+            self.G.node[v]['d'] = float('inf')  # distance from source
+            self.G.node[v]['p'] = None  # parent node
+        self.G.node[s]['d'] = 0
+
+    def _relax(self, u, v):
+        # Every edge should have a weight attribute         --> (1)
+        if self.G.node[v]['d'] > self.G.node[u]['d'] + self.G.edge[u][v]['weight']:  # if v.d > u.d + w(u, v)
+            self.G.node[v]['d'] = self.G.node[u]['d'] + self.G.edge[u][v]['weight']  # v.d = u.d + w(u, v)
+            self.G.node[v]['p'] = u  # set u as v's parent
+
+    def _region_for_latlng(self, R):
+        """
+        Transforms a region from lat-long system to block IDs
+        :param R: list of co-ordinates [nelat, nelong, swlat, swlong]
+        :return: region in terms of block IDs in the order - [ne, sw, se, nw]
+        """
+        ne = self._region_for(R[0], R[1])
+        sw = self._region_for(R[2], R[3])
+        se = self._region_for(R[2], R[1])
+        nw = self._region_for(R[0], R[3])
+        return [ne, sw, se, nw]
+
+    def _vertex_lies_in(self, v, R):
+        """
+        Checks if vertex v lies in region R
+        :param v: a spatial vertex in the Graph
+        :param R: list of co-ordinates [nelat, nelong, swlat, swlong]
+        :return: True if v lies in R, else False
+        """
+        lat = self.G.node[v]['spatial']['lat']
+        lng = self.G.node[v]['spatial']['lng']
+        if R[2] <= lat <= R[0] and R[3] <= lng <= R[1]:
+            return True
+        return False
+
+    def _vertex_reaches(self, u, R):
+        """
+        Checks if vertex 'u' can reach region 'R' using GeoReachPaths index
+        :param u: vertex whose index entry will be checked for its reachability to R
+        :param R: region in terms of block IDs in the order - [ne, sw, se, nw]
+        :return: True if reachable else False
+        """
+        ne, sw, se, nw = R
+        r = self._DEFAULT_RES
+        width = se - sw
+        for r in self.index[u]:  # TODO: Faster way to check if vertex u reaches region R
+            for block in range(sw, nw + 1, r):
+                if block <= r <= block + width:
+                    return True
+        return False
--- tests/Test_GeoReachPaths.py
+++ tests/Test_GeoReachPaths.py
@@ -31,8 +31,10 @@ def graph_fixture2():
     (A)-->(B) are friends
     (B)-->(C) are friends
     (C)-->(A) are friends
+    (C)-->(D) are friends
     (A)-->(R1), A checked in at R1
     (B)-->(R2), A checked in at R2
+    (D)-->(R3), D checked in at R3
     :return: networkx directed graph instance
     """
     G = nx.DiGraph()
@@ -40,16 +42,20 @@ def graph_fixture2():
     G.add_node('A')
     G.add_node('B')
     G.add_node('C')
+    G.add_node('D')
     # Add Businesses
     G.add_node('R1', spatial={'lat': 37.0, 'lng': 121.0})
     G.add_node('R2', spatial={'lat': -37.0, 'lng': -121.0})
+    G.add_node('R3', spatial={'lat': -24.0, 'lng': -156.0})
     # Add Friendships
     G.add_edge('A', 'B', weight=3.5)
     G.add_edge('B', 'C', weight=2.5)
     G.add_edge('C', 'A', weight=4.5)
+    G.add_edge('C', 'D', weight=2.5)
     # Add check-ins
     G.add_edge('A', 'R1', weight=4.0)
     G.add_edge('B', 'R2', weight=5.0)
+    G.add_edge('D', 'R3', weight=3.0)
     return G
 
 
@@ -60,5 +66,19 @@ def test_create_index():
     print t.create_index()
 
 
+def test_region():
+    G = nx.DiGraph()
+    G.add_node('A', spatial={'lat': 0, 'lng': 0})
+    G.add_node('B', spatial={'lat': -90, 'lng': -180})
+    G.add_node('C', spatial={'lat': 90, 'lng': 180})
+    t = GeoReachPaths(G, 2, 2)
+    setattr(t, '_DEFAULT_RES', 6)
+    assert t.region('A') is 55, 'incorrect A'
+    assert t.region('B') is 0, 'incorrect B'
+    assert t.region('C') is 99, 'incorrect C'
+    print 'Region Test Passed'
+
+
 if __name__ == '__main__':
     test_create_index()
+    # test_region()
